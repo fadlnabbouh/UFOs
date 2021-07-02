6a6dc449705c7648d8eed9a81335906d30f47281
fadlnabbouh <nabbouhf@gmail.com>
2021-07-02 10:31:46 -0400

    add code for table to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,20 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+// create buildTable function
+function buildTable(data) {
+    // clear table
+    tbody.html("");
+
+    // loop through each object and append a row for each value
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+
+        // loop through each field and add value to table cell
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
